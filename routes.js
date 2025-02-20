3456363297415d0f7fcd5c3c264ab7a560a61879
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-02-20 17:58:30 -0300

    feat(routes/init): initialize auth

--- routes.js
+++ routes.js
@@ -5,6 +5,8 @@ const { engine } = require("express-handlebars")
 const path = require("path")
 const sequelize = require("./sequelize/config.js")
 const Ip = require("./infra/ip.js")
+const MySql = require("./database/config.js")
+const User = require("./models/Users.js")
 
 
 app.use(express.json())
@@ -15,5 +17,44 @@ app.set("views", path.join(__dirname + "/views"))
 app.use(express.static(path.join(__dirname + "/static")))
 
 app.get("/", async(req, res)=>{
-  res.render("home")
+  const mysql = await MySql()
+  const ip = await Ip()
+
+  try {
+    const user = await User.findOne({
+      where: {
+        ip: ip
+      }
+    })
+    if(user===null){
+      res.render("home")
+
+      console.log({
+        message: "User not found",
+        error: "User not exists",
+        redirecting: "/login"
+      })
+    }else{
+      res.render("home", {
+        userName: user["nome"]
+      })
+      // success message
+      console.log({
+        message: "Successiful",
+        userName: user["nome"]
+      })
+
+      // console.log({
+      //   message: "Successiful",
+      //   userName: user["nome"]
+      // })
+    }
+  }catch(error){
+    console.error("Error fetching user:", error)
+    res.status(500).json({
+      message: "Error fetching user",
+      error: error
+    })
+  }
+
 })
