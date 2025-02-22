c94ad2537a7c3393a0dcf45855f26c2564e2ddcf
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-02-22 15:58:24 -0300

    build: onrender building

--- routes.js
+++ routes.js
@@ -7,6 +7,9 @@ const sequelize = require("./sequelize/config.js")
 const Ip = require("./infra/ip.js")
 const MySql = require("./database/config.js")
 const User = require("./models/Users.js")
+const formatName = require("./funcs/formating.js")
+const { marked } = require("marked")
+const date = require("./moment/date.js")
 
 
 app.use(express.json())
@@ -142,6 +145,98 @@ app.post("/login", async(req, res)=>{
       })
 
       res.redirect('/')
+    }
+  }catch(error){
+    console.error("Error fetching user:", error)
+    res.status(500).json({
+      message: "Error fetching user",
+      error: error
+    })
+  }
+})
+app.get("/cadastro", async(req, res)=>{
+  const ip = await Ip()
+  
+  try{
+    const user = await User.findOne({
+      where: {
+        ip
+      }
+    })
+
+    if(user === null){
+      res.render("cadastro")
+    }else{
+      res.redirect("/")
+      console.log({
+        message: "User already exists",
+        userName: user["nome"]
+      })
+    }
+  }catch(error){
+    console.error("Error fetching user:", error)
+    res.status(500).json({
+      message: "Error fetching user",
+      error: error
+    })
+  }
+})
+app.post("/cadastro", async(req, res)=>{
+  const ip = await Ip()
+  const { email, senha, curso, status, data_nasc } = req.body
+  const nome = formatName(req.body.nome)
+  
+  try{
+    const user = await User.findOne({
+      where: {
+        email,
+        nome
+      }
+    })
+
+    if(user === null){
+      const userCreate = await User.create({
+        nome,
+        email,
+        senha,
+        bio: marked("Olá amigos"),
+        curso,
+        status,
+        data_nasc,
+        ip,
+        data: date
+      })
+
+      console.log({
+        message: "Success",
+        userCreate
+      })
+      res.redirect('/')
+    }else{
+      res.render("cadastro", {
+        notify: `
+          <div
+            class="alert alert-danger alert-dismissible fade show"
+            role="alert"
+          >
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="alert"
+              aria-label="Close"
+            ></button>
+            <h2>
+              <strong>
+                Aviso!!
+              </strong>
+            </h2>
+            <hr>
+            <span>E-mail ou nome de usuário já cadastrados! Por favor, insira dados válidos para poder cadastrar-se.</span>
+            <br>
+          </div>
+          <br>
+        `
+      })
     }
   }catch(error){
     console.error("Error fetching user:", error)
