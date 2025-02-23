e81205ac81f343a8ad2389ee8bf594e27999c564
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-02-23 16:50:42 -0300

    feat(upload/files): uploading files and images

---
+++ upload/files.js
@@ -0,0 +1,20 @@
+const multer = require("multer")
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb)=>{
+    cb(null, "../images/")
+  },
+  filename: (req, file, cb)=>{
+    const fileExt = file.originalname.split(".")[1]
+
+    const newFileName = require("crypto")
+      .randomBytes(64)
+      .toString("hex")
+
+    cb(null, `${newFileName}.${fileExt}`)
+  }
+})
+
+const upload = multer({ storage })
+
+module.exports = upload
